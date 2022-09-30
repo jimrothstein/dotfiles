4856f2c306bf3611484398260eb9c90d25a4c0ef
jimrothstein <jimrothstein@gmail.com>
2022-09-30 03:05:14 -0700

    wip

--- options.lua
+++ options.lua
@@ -113,6 +113,9 @@ vim.cmd [[
 	set clipboard+=unnamedplus
 	:let g:loaded_netrw       = 1
 	:let g:loaded_netrwPlugin = 1
+	" checkhealth:  turns off python & perl warning
+	:let g:loaded_python3_provider = 0
+	:let g:loaded_perl_provider = 0
 	]]
 
 		
