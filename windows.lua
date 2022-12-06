621f238b305d9254e6ad37a9b69992ab57c7c927
jimrothstein <jimrothstein@gmail.com>
2022-12-06 00:14:09 -0800

    wip

--- windows.lua
+++ windows.lua
@@ -16,16 +16,18 @@ nnoremap <leader>u1 :call UnderlineHeading(1)<cr>
 nnoremap <leader>u2 :call UnderlineHeading(2)<cr>
 nnoremap <leader>u3 :call UnderlineHeading(3)<cr>
 
-
+"---------------------------------------
+" Nov 30, 2022, jr stops tabs;   USE buffers"
 "	tabs, begins with 1
 
-tabnew  ~/code/docs/300_tech_notes.md
-tabnew	~/code/docs/001_tech_reading.md
-tabnew	~/code/docs/medical_notes.md
-tabfirst
+" tabnew  ~/code/docs/300_tech_notes.md
+" tabnew	~/code/docs/001_tech_reading.md
+" tabnew	~/code/docs/medical_notes.md
+" tabfirst
+"---------------------------------------
 
 "	split and load $V
-vsplit $V
+"vsplit $V
 "	wincmd R
 
 ]]
