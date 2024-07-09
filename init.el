469c77f4879a431fec5eb243fc768be06b386573
jimrothstein <jimrothstein@gmail.com>
2024-07-08 17:05:18 -0700

    wip

--- init.el
+++ init.el
@@ -146,6 +146,17 @@
   (interactive)
   (other-window -1))
 
+;; short-cuts
+;; (use-package dired
+;;   ;; built-in don't install !
+;;   :ensure nil
+;;   :commands (dired dired-jump)
+;;   :bind (("C-x C-j" . dired-jump))
+;;   :custom ((dired-listing-switches "-agho --group-directories-first"))
+;;   :config
+;;   (evil-collection-define-key 'normal 'dired-mode-map
+;;     "h" 'dired-single-up-directory
+;;     "l" 'dired-single-buffer))
 
 ;;(use-package ess-smart-underscore
 ;;  :ensure t
