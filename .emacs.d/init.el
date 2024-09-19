e7bd6746a9729738d10f88f075e50b160a040b3a
jimrothstein <jimrothstein@gmail.com>
2024-09-18 17:39:09 -0700

    wip

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,17 +18,16 @@
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
 
+
 (use-package yasnippet
   :ensure t)
 (yas-global-mode 1)
-
 (use-package org
   :ensure t)
 
-
+;; evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
-
 (use-package evil
   :ensure t
   :config
@@ -38,8 +37,9 @@
 ;;  (define-key evil-insert-state-map "jj" 'evil-normal-state)
   (define-key evil-normal-state-map ",w" 'save-buffer)
 
-
+;; use-package
 (use-package ess)
+
 
 ;; replace flymake with flycheck
 (setq ess-use-flymake nil)
@@ -85,6 +85,8 @@
 
 ;; registers (short-cuts to files)
 (set-register ?i (cons 'file "~/.emacs.d/init.el"))
+(set-register ?e (cons 'file "~/code/docs/tech_notes/280_emacs_notes.qmd"))
+
 (set-register ?j (cons 'file "~/code/docs/jobs/310_job_notes.qmd"))
 (set-register ?p (cons 'file "~/code/docs/tech_notes/810_pharmaverse_NOTES.org"))
 (set-register ?t (cons 'file "~/code/docs/tech_notes/300_tech_notes.org"))
