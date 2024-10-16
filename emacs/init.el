600e4e46340731ebec67b21afabfe1f972894876
jimrothstein <jimrothstein@gmail.com>
2024-10-15 19:44:17 -0700

    wip

--- emacs/init.el
+++ emacs/init.el
@@ -94,6 +94,7 @@
 (set-register ?i (cons 'file "~/.config/emacs/init.el"))
 (set-register ?e (cons 'file "~/code/docs/tech_notes/280_emacs_notes.qmd"))
 (set-register ?j (cons 'file "~/code/docs/jobs/310_job_notes.qmd"))
+(set-register ?m (cons 'file "~/code/docs/health_notes/medical_notes.org"))
 (set-register ?p (cons 'file "~/code/docs/tech_notes/810_pharmaverse_NOTES.org"))
 (set-register ?q (cons 'file "~/code/docs/tech_notes/250_quarto_knitr_SCSS_NOTES.qmd"))
 (set-register ?r (cons 'file "~/code/docs/tech_notes/310_R_notes.qmd"))
