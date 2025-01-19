c7ed89f3e1cca97228585e2f0f4138bcc911f4fa
jimrothstein <jimrothstein@gmail.com>
2025-01-18 23:42:29 -0800

    wip

--- aliases.zsh
+++ aliases.zsh
@@ -1,12 +1,9 @@
-# Set personal aliases, overriding those provided by oh-my-zsh 
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# Set personal zsh aliases
 # For a full list of active aliases, run `alias`.
 #
 # (jr) To find WHERE alias defined:
 # >which <alias>
 # >grep -r '<alias=xxxx>'
-# 
 #
 # TODO:  in this file, wrap all alias calls in a function()
 # from .zshrc,  call this function
