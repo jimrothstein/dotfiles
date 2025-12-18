f0cbfae76eaf7da60d727bea3e029a8bca350403
jimrothstein <jimrothstein@gmail.com>
2025-12-17 20:26:08 -0800

    wip

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,6 @@
 ;; package -- summary: thereafter use-package loads packages  toggle whitespace mode on/off (now in init.el)
 
+;; Timestamp: <>
 ;;------------------------ setup
 
 ;; default package mgr:  `package`
@@ -455,4 +456,15 @@
 
 ; ESS, R clear console (console MUST have focus)
 (global-set-key (kbd "C-l") 'comint-clear-buffer) ; clear R console/process buffer
+
+
+;;------------------------  time-stamp
+;; REF: https://olddeuteronomy.github.io/post/emacs-time-stamp/
+;; When there is a "Time-stamp: <>" string in the first 10 lines of the file,
+;; Emacs will write time-stamp information there when saving the file.
+;; (Borrowed from http://home.thep.lu.se/~karlf/emacs.html)
+(setq time-stamp-active t          ; Do enable time-stamps.
+      time-stamp-line-limit 10     ; Check first 10 buffer lines for Time-stamp: <>
+      time-stamp-format "Last changed %Y-%02m-%02d %02H:%02M:%02S by %u")
+(add-hook 'write-file-hooks 'time-stamp) ; Update when saving.
 
